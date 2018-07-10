9168c5505f6179585b6b7a07acb9ebd88da691cd
Robin Winslow <robin@robinwinslow.co.uk>
2018-07-10 10:38:01 +0100

    v0.1.0: Add session classes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canonicalwebteam.http',
-    version='0.0.0',
+    version='0.1.0',
     author='Canonical webteam',
     author_email='webteam@canonical.com',
     url='https://github.com/canonicalwebteam/http',
