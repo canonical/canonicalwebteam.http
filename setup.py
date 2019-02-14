237684cce094b06f472a30964ee10c84b453652d
Max <2843450+b-m-f@users.noreply.github.com>
2019-02-14 14:51:15 +0000

    Use system default locale, instead of setting it explicitely (#15)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.http",
-    version="1.0.0",
+    version="1.0.1",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/http",
