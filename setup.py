d208c71a830af9bc0bb4256a3862ecc351587b8c
Robin Winslow <robin@robinwinslow.co.uk>
2023-06-05 15:58:22 +0100

    Update version to 1.0.4: Filecache dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.http",
-    version="1.0.3",
+    version="1.0.4",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/http",
